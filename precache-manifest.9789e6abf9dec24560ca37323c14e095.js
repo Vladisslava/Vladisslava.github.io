81b0638fbf0bbd9100733907cea08ff723e693d5
Antsiferov Maxim <antsiferovmaximv@gmail.com>
2019-04-15 15:23:56 +0300

    Add app

---
+++ precache-manifest.9789e6abf9dec24560ca37323c14e095.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b8969124370f0866cdfb94324e4336",
+    "url": "/static/media/surprise.12b89691.png"
+  },
+  {
+    "revision": "0305476158eb79567b98",
+    "url": "/static/css/main.8e8814f5.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "9c3608d537ec44a91e5c",
+    "url": "/static/js/2.0d151e9f.chunk.js"
+  },
+  {
+    "revision": "50578062010bb8730e3106c496ffc810",
+    "url": "/static/media/MyriadPro.50578062.ttf"
+  },
+  {
+    "revision": "d87e1daa65ff0620b5dc1e3c22e6eeed",
+    "url": "/static/media/1.d87e1daa.jpg"
+  },
+  {
+    "revision": "0305476158eb79567b98",
+    "url": "/static/js/main.defa63cf.chunk.js"
+  },
+  {
+    "revision": "8808b6ef01f76246b6ec2a6b706b2402",
+    "url": "/static/media/img.8808b6ef.png"
+  },
+  {
+    "revision": "9c9bf3bdaf4dc59cbf7af39b13f5658b",
+    "url": "/static/media/menu.9c9bf3bd.png"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "9c3608d537ec44a91e5c",
+    "url": "/static/css/2.0688291a.chunk.css"
+  },
+  {
+    "revision": "9f78520c6aad34ff20b9ba911acb0135",
+    "url": "/index.html"
+  }
+];
